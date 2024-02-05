3f6825198db2bb2abb2172c2d7322609552a4961
Jawad Ali <jawadali000123@gmail.com>
2024-02-06 00:41:20 +0500

    Delete backend directory

--- backend/src/errors/custom-error.ts
+++
@@ -1,8 +0,0 @@
-class CustomError extends Error {
-  constructor(message: string) {
-    super(message);
-    Object.setPrototypeOf(this, CustomError.prototype);
-  }
-}
-
-export default CustomError;
