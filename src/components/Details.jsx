ab025267d649f2c56a7da6e01df1862d591a2873
notjawad404 <jawadali000123@gmail.com>
2024-02-08 01:53:45 +0500

    Update Details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Details() {
     const [recipes, setRecipes] = useState([]);
@@ -21,6 +21,8 @@ export default function Details() {
                     const data = await response.json();
                     const filterData = data.hits.map(hit => hit.recipe).filter(recipe => recipe.uri === uri);
                     setRecipes(filterData);
+
+                    defaultData();
                 } else {
                     console.error("Label or URI is undefined.");
                 }
@@ -33,6 +35,11 @@ export default function Details() {
         fetchData();
     },[]);
 
+    const defaultData = () => {
+        setShowdata1(true);
+        setShowdata2(false);
+        setShowdata3(false);
+    }
 
     const showIngredients = () => {
         setShowdata1(true);
@@ -54,38 +61,42 @@ export default function Details() {
 
 
   return (
-    <div className="h-screen bg-slate-700">
-      <div>
-        <h1>Welcome to Recipe App</h1>
+    <div className="h-screen overflow-y-auto bg-slate-700 font-sans">
+      <div className=" text-white flex flex-row py-2">
+        <Link to='/' className="text-xl px-5">Home</Link>
+        <h1 className="lg:text-3xl px-5 text-center">Foodie Hub</h1>
+        
       </div>
       <div className=" w-3/4 mx-auto bg-white shadow-2xl">
         <div className="">
             {recipes.map((recipe, index) => (
-                <div key={index} className="flex flex-row">
-                    <div className="w-1/2 bg-gradient-to-r to-slate-600 from-red-700">
-                    <h2>{recipe.label}</h2>
+                <div key={index} className="flex lg:flex-row flex-col">
+                    <div className="lg:w-1/2 w-full lg:bg-gradient-to-l bg-gradient-to-t to-slate-900 from-slate-600 text-white p-3">
+                    <h2 className="text-center text-lg py-1">{recipe.label}</h2>
                     
-                    <img src={recipe.image} alt={recipe.label} />
-                    <p><span>Calories: </span> {recipe.calories.toFixed(2)}</p>
-                    <p><span>Yield: </span> {recipe.yield}</p>
-                    <p><span>Cuisine Type: </span> {recipe.cuisineType.join(', ')}</p>
-                    <p><span>Diet Labels: </span> {recipe.dietLabels.join(', ')}</p>
+                    <img src={recipe.image} alt={recipe.label} className="rounded-lg"/>
+                    <p className="text-sm py-1"><span className="text-lg">Calories: </span> {recipe.calories.toFixed(2)}</p>
+                    <p className="text-sm py-1"><span className="text-lg">Cuisine Type: </span> {recipe.cuisineType.join(', ')}</p>
+                    <p className="text-sm py-1"><span className="text-lg">Diet Labels: </span> {recipe.dietLabels.join(', ')}</p>
+                    <p className="text-sm py-1"><span className="text-lg">Source: </span> <Link to={recipe.url} target="_blank" rel="noopener noreferrer" className=' text-blue-600 px-2 py-1'>{recipe.source}</Link></p>
+
+
                     <div className="flex flex-col">
-                    <button className=" bg-slate-700 text-white shadow-lg p-2 mx-1 my-2 rounded-3xl" onClick={showIngredients} >Ingredients</button>
-                    <button className=" bg-slate-700 text-white shadow-lg p-2 mx-1 my-2 rounded-3xl" onClick={showNutritional} >Nutritional Info</button>
-                    <button className=" bg-slate-700 text-white shadow-lg p-2 mx-1 my-2 rounded-3xl" onClick={showHealthLabel} >Health Labels</button>
+                    <button className=" bg-slate-300 text-black shadow-lg p-2 mx-1 my-2 rounded-3xl w-3/4" onClick={showIngredients} >Ingredients</button>
+                    <button className=" bg-slate-300 text-black shadow-lg p-2 mx-1 my-2 rounded-3xl w-3/4" onClick={showNutritional} >Nutritional Info</button>
+                    <button className=" bg-slate-300 text-black shadow-lg p-2 mx-1 my-2 rounded-3xl w-3/4" onClick={showHealthLabel} >Health Labels</button>
                     </div>
 
                     </div>
 
-                    <div className=" w-1/2 bg-gradient-to-l to-slate-600 from-red-700 text-white">
+                    <div className=" lg:w-1/2 w-full lg:bg-gradient-to-r bg-gradient-to-b to-slate-900 from-slate-600 text-white text-center">
                     {/* Ingredients */}
                     {showdata1 && (
-                        <div className=" h-fit overflow-y-auto">
-                            <h3>Ingredients</h3>
+                        <div className="text-lg font-semibold">
+                            <h3 className="">Ingredients</h3>
                             <ul>
                                 {recipe.ingredientLines.map((ingredient, idx) => (
-                                    <li key={idx}>{ingredient}</li>
+                                    <li key={idx} className="border-x-2 border-y-2 border-gray-400">{ingredient}</li>
                                 ))}
                             </ul>
                         </div>
@@ -93,27 +104,27 @@ export default function Details() {
 
                     {/* Nutritional Info */}
                     {showdata2 && (
-                        <div className=" h-4/5 overflow-y-auto">
+                        <div className="text-lg font-semibold">
                             <h3 className="text-center">Nutritional Information</h3>
-                            <ul>
+                            <ol>
                                 {Object.entries(recipe.totalNutrients).map(([key, value]) => (
-                                    <li key={key}>{value.label}: {value.quantity.toFixed(2)} {value.unit}</li>
+                                    <li key={key} className="border-x-2 border-y-2 border-gray-400"><span className="text-lg ">{value.label}: </span><span className="text-sm">{value.quantity.toFixed(2)}</span> <span className="text-sm"> {value.unit}</span></li>
                                 ))}
-                            </ul>
+                            </ol>
                         </div>
                     )}
 
                     {/* Health Labels */}
-                    {showdata3 && (
+                    {showdata3 &&  (
                         <div>
-                            <h3>Health Labels</h3>
-                            <ul>
+                            <h3 className="text-center text-lg font-semibold">Health Labels</h3>
+                            <ol className="">
                                 {recipe.healthLabels.map((label, idx) => (
-                                    <li key={idx}>{label}</li>
+                                    <li key={idx} className="border-x-2 border-y-2 border-gray-400">{label}</li>
                                 ))}
-                            </ul>
+                            </ol>
                         </div>
-                    )}
+                    )} 
                     
 
                     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import '../App.css'
 
 import { useState } from "react";
@@ -23,35 +22,33 @@ export default function Home() {
     }
     const navigate = useNavigate();
     const sendData = (label, uri) => {
-        console.log(label, uri);
         navigate('/details', {state: {label, uri}});
         
 
     }
 
     return (
-        <div className='h-screen overflow-y-auto'>
-            <h1 className="text-center font-semibold text-3xl">Welcome to Recipe App</h1>
+        <div className='h-screen overflow-y-auto bg-slate-700'>
+            <h1 className="text-center font-semibold text-3xl text-white">Welcome to Recipe App</h1>
             <div className="flex justify-center mt-10">
                 <form onSubmit={(e) => { e.preventDefault(); SearchRecipe(foodItem); }}>
-                    <input value={foodItem} onChange={(e) => setFoodItem(e.target.value)} type="text" className="p-4 w-80 text-black bg-slate-200 shadow-2xl rounded-3xl" />
+                    <input value={foodItem} onChange={(e) => setFoodItem(e.target.value)} placeholder='Search Recipe' type="text" className="p-4 w-80 text-black bg-slate-200 shadow-2xl rounded-3xl" />
                 </form>
             </div>
 
             <div className='flex flex-wrap justify-around py-5'>
                 {recipes.map((recipe, index) => (
-                    <div key={index} className='w-1/4 flex justify-between px-2 my-2'>
+                    <div key={index} className='lg:w-1/4 md:w-1/3 sm:w-1/2 flex justify-between px-2 my-2'>
 
-                        <div className='bg-whie shadow-2xl rounded-2xl w-full h-80'>
+                        <div className='bg-slate-400 shadow-2xl rounded-2xl w-full h-72'>
                             <div className=' bg-slate-200 shadow-md rounded-2xl'>
                                 <img src={recipe.image} alt={recipe.label} className=' w-96 p-2 h-52 rounded-2xl shadow-sm' />
-
                             </div>
 
                             <h2 className='text-lg font-medium mb-2 text-nowrap overflow-hidden'>
                                 <span className='hover:text-wrap'>{recipe.label}</span>
                             </h2>
-                            <Link to={recipe.url} target="_blank" rel="noopener noreferrer" className=' text-blue-600 px-2 py-1'>{recipe.source}</Link><br></br>
+
                             <button onClick={()=>sendData(recipe.label, recipe.uri)} className=' bg-slate-800 text-white p-1 rounded-lg mx-2'>
                                  View Recipe
                             </button>
