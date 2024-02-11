bfbc88af190afa12c79124c7c337e08c5712edb7
notjawad404 <jawadali000123@gmail.com>
2024-02-11 18:02:39 +0500

    Add nested routes

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -5,7 +5,7 @@ import Login from "../auth/Login";
 import Register from "../auth/Register";
 import Details from "../Details";
 import Home from "../Home";
-import NotFoundPage from "../NotFoundPage";
+// import NotFoundPage from "../NotFoundPage";
 import ProtectedRoutes from "./ProtectedRoutes";
 import App from "../../App";
 
@@ -14,15 +14,16 @@ const router = BrowserRouter(
 
         <Route path="/" element={<App/>}>
         <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
         <Route path="/details" element={<Details/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/user-profile" element={<UserProfile/>} />
-        <Route path="/" element={<ProtectedRoutes/>}>
-        <Route path="/favourites" element={<Favourites/>} />
-        </Route>
+        <ProtectedRoutes>
+        <Route path="/favourites" element={<Favourites />} />
+      </ProtectedRoutes>
 
-        <Route path="*" element={<NotFoundPage/>} />
+        <Route path="*" element={<h1>ABCS</h1>} />
         </Route>
     
     )
